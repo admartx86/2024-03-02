12e7ed5a17fbc60f27677b81e8e742b1545ca9b2
Adam Martinez <12470793+mart3318@users.noreply.github.com>
2024-03-02 15:13:07 -0600

    feat: add user-facing error messages to SignInForm

--- src/app/sign-in/SignInForm.tsx
+++ src/app/sign-in/SignInForm.tsx
@@ -1,18 +1,28 @@
 'use client'
 
-import { useState, FormEvent, ChangeEvent } from 'react';
+import { useState, useEffect, FormEvent, ChangeEvent } from 'react';
 
 type FormState = {
     signInUsername: string;
     signInPassword: string;
 }
 
+type ErrorState = {
+    usernameRequired: boolean;
+    passwordRequired: boolean;
+};
+
 export default function SignInForm() {
     
     const [form, setForm] = useState<FormState>({
         signInUsername: '',
-        signInPassword: ''
+        signInPassword: '',
     })
+
+    const [errors, setErrors] = useState<ErrorState>({
+        usernameRequired: true,
+        passwordRequired: true,
+    });
 
     const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
         setForm({
@@ -23,26 +33,82 @@ export default function SignInForm() {
 
     const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
+        alert(form.signInUsername + ' ' + form.signInPassword);
         setForm({
             signInUsername: '',
             signInPassword: '',
         });
-        alert(form.signInUsername + ' ' + form.signInPassword);
+        setErrors({
+            usernameRequired: true,
+            passwordRequired: true,
+        });
     };
+
+    useEffect( () => {
+        if (form.signInUsername.trim() === '') {
+            setErrors({
+                ...errors,
+                usernameRequired: true,
+            });
+        } else {
+            setErrors({
+                ...errors,
+                usernameRequired: false,
+            });
+        }; 
+    }, [form.signInUsername]);
+
+    useEffect( () => {
+        if (form.signInPassword.trim() === '') {
+            setErrors({
+                ...errors,
+                passwordRequired: true,
+            });
+        } else {
+            setErrors({
+                ...errors,
+                passwordRequired: false,
+            });
+        }; 
+    }, [form.signInPassword]);
 
     return(
         <form onSubmit={handleSubmit}>
             <div>
                 <label htmlFor='signInUsername'>Username</label>
-                <input id='signInUsername' type='text' value={form.signInUsername} onChange={handleChange}></input>
+                <input className='border-black border-2' id='signInUsername' type='text' value={form.signInUsername} onChange={handleChange}></input>
+                {errors.usernameRequired === true ?
+                    <div>
+                        <span className='text-gray-500'>A username is required. Please enter your username.</span>
+                    </div>
+                :
+                    <div> 
+                        <span>A username is required. Please enter your username. </span>
+                    </div>
+                }   
             </div>
             <div>
                 <label htmlFor='signInPassword'>Password</label>
-                <input id='signInPassword' type='password' value={form.signInPassword} onChange={handleChange}></input>
+                <input className='border-black border-2' id='signInPassword' type='password' value={form.signInPassword} onChange={handleChange}></input>
+                {errors.passwordRequired === true ?
+                    <div>
+                        <span className='text-gray-500'>A password is required. Please enter your password.</span>
+                    </div>
+                : 
+                    <div>
+                        <span>A password is required. Please enter your password. </span>
+                    </div>
+                }
             </div>
-            <button type='submit'>
-                Submit
-            </button>
+            {errors.usernameRequired === true || errors.passwordRequired === true ?
+                <button disabled={true} type='submit' className='border-black border-2 text-gray-500'>
+                    Submit
+                </button>
+            :
+                <button type='submit' className='border-black border-2'>
+                    Submit
+                </button>
+            }
         </form>
     );
 };
